a34784b69eb09774ff2b75fc115d086cfe13690f
emirefek <emirefek@gmail.com>
2023-10-07 16:00:14 +0300

    trpc setup for client

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,20 +1,14 @@
-import { useState } from 'react'
-import { RouterProvider } from 'react-router-dom';
-import router from './Routes/route';
-import React from 'react';
-import { MantineProvider, createTheme } from '@mantine/core';
-const theme = createTheme({
-
-});
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/route";
+import { MantineProvider, createTheme } from "@mantine/core";
+const theme = createTheme({});
 
 function App() {
   return (
     <MantineProvider theme={theme}>
       <RouterProvider router={router} />
     </MantineProvider>
-
-    
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ apps/client/src/context/TrpcProvider.tsx
@@ -0,0 +1,31 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { trpc } from "../lib/trpc";
+import { ReactNode, useState } from "react";
+import { httpBatchLink } from "@trpc/client";
+
+export default function TrpcProvider(props: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: `${import.meta.env.VITE_API_URL}/trpc`,
+          // You can pass any HTTP headers you wish here
+          // async headers() {
+          //   return {
+          //     authorization: getAuthCookie(),
+          //   };
+          // },
+        }),
+      ],
+    })
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        {props.children}
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
